d7064e3f0693e90e56cd201865a07c967d5f0163
William Bodily <william.bodily@gmail.com>
2018-07-29 23:06:46 -0400

    Added a collection view cell UI

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by William Bodily on 7/29/18.
+//  Copyright © 2018 William Bodily. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
